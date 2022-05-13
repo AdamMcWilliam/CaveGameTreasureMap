ceeb423cbc04a247cfdf7fd5116aa53814738529
McCroden <stupac62@gmail.com>
2022-05-13 14:42:02 -0500

    clean up code

--- spritesheetSplit.py
+++ spritesheetSplit.py
@@ -1,15 +1,18 @@
 from PIL import Image
-from os import mkdir
 
-#mkdir("cavesprites/")
-sheet = Image.open("cavesprites.png")
-count = 0
 
-for y in range(4):
-    for x in range(16):
-        a = (x + 1) * 66
-        b = (y + 1) * 66
-        icon = sheet.crop((a - 66, b - 66, a, b))  # Problem here
-        icon = icon.resize((33,33))
-        icon.save(f"cavesprites/{count}.png")
-        count += 1
+OG_SPRITE_SIZE = 66
+
+
+def create_sprites(size: int) -> None:
+    sheet = Image.open("cavesprites.png")
+    count = 0
+    # sprite sheet has 4 rows and 16 cols
+    for y in range(4):
+        for x in range(16):
+            a = (x + 1) * OG_SPRITE_SIZE
+            b = (y + 1) * OG_SPRITE_SIZE
+            sprite = sheet.crop((a - OG_SPRITE_SIZE, b - OG_SPRITE_SIZE, a, b))
+            sprite = sprite.resize((size, size))
+            sprite.save(f"cavesprites/{count}.png")
+            count += 1
